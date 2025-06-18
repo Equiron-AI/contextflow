963ce3ae29df5d78e723b266330fa32d2f31208b
man4j <man4j@ya.ru>
2025-06-18 23:38:54 +0300

    fixes

--- src/contextflow/backends.py
+++ src/contextflow/backends.py
@@ -22,14 +22,14 @@ class LlamaCppBackend:
                 "cache_prompt": True}
 
     def completion(self, request_tokens, temp=0.7, top_p=0.95, min_p=0.05, top_k=20, dry_multiplier=0.0):
-        request = self.get_request_object(request_tokens, False, temp, top_p, min_p, dry_multiplier)
+        request = self.get_request_object(request_tokens, False, temp, top_p, min_p, top_k, dry_multiplier)
         response = requests.post(self.url, json=request)
         response.raise_for_status()
         resp_obj = response.json()
         return resp_obj["content"], resp_obj["stop_type"]
 
     async def async_completion(self, request_tokens, temp=0.7, top_p=0.95, min_p=0.05, top_k=20, dry_multiplier=0.0, callback=None):
-        request = self.get_request_object(request_tokens, True, temp, top_p, min_p, dry_multiplier)
+        request = self.get_request_object(request_tokens, True, temp, top_p, min_p, top_k, dry_multiplier)
         response = requests.post(self.url, json=request, stream=True, headers={'Accept': 'text/event-stream'})
         response.raise_for_status()
         stream = sseclient.SSEClient(response).events()
