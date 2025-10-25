75c26f9283e3426156b1b577ce3e40abd1fc1b51
man4j <man4j@ya.ru>
2025-10-25 17:00:06 +0300

    fixes

--- src/contextflow/backends.py
+++ src/contextflow/backends.py
@@ -8,29 +8,26 @@ class LlamaCppBackend:
         self.url = url
         self.max_predict = max_predict
 
-    def get_request_object(self, request_tokens, stream, temp, top_p, min_p, top_k, dry_multiplier):
+    def get_request_object(self, request_tokens, stream, temp, top_p, min_p, top_k, dry_multiplier, id_slot):
         return {"prompt": request_tokens,
-                "stream": stream,
+                "stream": True,
                 "n_predict": self.max_predict,
                 "temperature": temp,
                 "top_p": top_p,
                 "min_p": min_p,
                 "top_k": top_k,
                 "stop": [self.stop_token, self.tokenizer.eos_token],
-                "dry_multiplier": dry_multiplier,
-                "dry_sequence_breakers": ["\n", ":", "\"", "*", "|", ""],
-                "cache_prompt": True}
+                "id_slot": id_slot}
 
-    def completion(self, request_tokens, temp=0.7, top_p=0.95, min_p=0.05, top_k=20, dry_multiplier=0.0):
-        request = self.get_request_object(request_tokens, False, temp, top_p, min_p, top_k, dry_multiplier)
-        response = requests.post(self.url, json=request)
+    def total_slots(self):
+        response = requests.get(self.url + "/props")
         response.raise_for_status()
         resp_obj = response.json()
-        return resp_obj["content"], resp_obj["stop_type"]
+        return resp_obj["total_slots"]
 
-    async def async_completion(self, request_tokens, temp=0.7, top_p=0.95, min_p=0.05, top_k=20, dry_multiplier=0.0, callback=None):
-        request = self.get_request_object(request_tokens, True, temp, top_p, min_p, top_k, dry_multiplier)
-        response = requests.post(self.url, json=request, stream=True, headers={'Accept': 'text/event-stream'})
+    async def async_completion(self, request_tokens, temp=0.7, top_p=0.95, min_p=0.05, top_k=20, id_slot=-1, callback=None):
+        request = self.get_request_object(request_tokens, True, temp, top_p, min_p, top_k, id_slot)
+        response = requests.post(self.url + "/completion", json=request, headers={'Accept': 'text/event-stream'})
         response.raise_for_status()
         stream = sseclient.SSEClient(response).events()
         text_resp = ""
