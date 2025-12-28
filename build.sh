0e8d660cf76ce76bdab756920efe1f18f4559aea
root <root@srv952431322.host>
2025-12-28 08:34:22 +0000

    fixes

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 rm -rf build dist *.egg-info
-python -m build
-python -m twine check dist/*
-python -m twine upload dist/*
 rm -rf ./dist
 rm -rf ./src/tuningtron.egg-info
 rm -rf ./src/tuningtron/__pycache__
+
+python -m build
+python -m twine check dist/*
+python -m twine upload dist/*
 
